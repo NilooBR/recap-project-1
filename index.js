05aa5b465028495166bb3fa39b709abc1f392798
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-11 09:13:20 +0100

    added dark mode button to profile

---
+++ index.js
@@ -0,0 +1,10 @@
+console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeButton = document.querySelector('[data-js="dark-mode"]');
+
+darkModeButton.addEventListener('click', () => {
+    bodyElement.classList.toggle('dark');
+  });
+  
+  

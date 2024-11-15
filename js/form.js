8d140295e52ae21590fd9c7b53b8319f000078f0
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-15 13:12:19 +0100

    create the form dynamically when the page loads

---
+++ js/form.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.createElement("form");
+
+  // "Question"
+  const questionLabel = document.createElement("label");
+  questionLabel.textContent = "Question";
+  questionLabel.setAttribute("for", "question");
+
+  const questionTextarea = document.createElement("textarea");
+  questionTextarea.setAttribute("id", "question");
+  questionTextarea.setAttribute("name", "question");
+
+  // "Answer"
+  const answerLabel = document.createElement("label");
+  answerLabel.textContent = "Answer";
+  answerLabel.setAttribute("for", "answer");
+
+  const answerTextarea = document.createElement("textarea");
+  answerTextarea.setAttribute("id", "answer");
+  answerTextarea.setAttribute("name", "answer");
+
+  //"Tag"
+  const tagLabel = document.createElement("label");
+  tagLabel.textContent = "Tag";
+  tagLabel.setAttribute("for", "tag");
+
+  const tagInput = document.createElement("input");
+  tagInput.setAttribute("id", "tag");
+  tagInput.setAttribute("name", "tag");
+  tagInput.setAttribute("type", "text");
+
+  // Submit button
+  const createButton = document.createElement("button");
+  createButton.textContent = "Create";
+  createButton.setAttribute("type", "submit");
+
+  // Append to the form
+  form.appendChild(questionLabel);
+  form.appendChild(questionTextarea);
+  form.appendChild(answerLabel);
+  form.appendChild(answerTextarea);
+  form.appendChild(tagLabel);
+  form.appendChild(tagInput);
+  form.appendChild(createButton);
+
+  // Append the form to the page
+  const formContainer = document.getElementById("form-container");
+  formContainer.appendChild(form);
+});
